2c4f877828b4b6dfb28ecdcd9cb3327a3e8706c4
grimm <jason@gina.alaska.edu>
2013-04-22 17:33:55 -0800

    Fixed for sure now !?

--- lib/rvg_graph/graph_helpers.rb
+++ lib/rvg_graph/graph_helpers.rb
@@ -1,11 +1,13 @@
-module RvgGraph::GraphHelpers
-  def check_no_data(value, no_data)
-    if value.is_a? Numeric
-      return true if value == no_data.to_f
-    else
-      return true if value == no_data
-    end
+module RvgGraph
+  module GraphHelpers
+    def check_no_data(value, no_data)
+      if value.is_a? Numeric
+        return true if value == no_data.to_f
+      else
+        return true if value == no_data
+      end
 
-    false
+      false
+    end
   end
 end
