540c6d971c9c840feae1a444ceb638bddc55e910
grimm <jason@gina.alaska.edu>
2014-06-10 17:08:21 -0800

    Fix the aggregate function.

--- lib/rvg_graph/agg.rb
+++ lib/rvg_graph/agg.rb
@@ -17,8 +17,8 @@ module RvgGraph
         unless check_no_data(value, no_data_value)
           self.maxval = value if self.maxval.nil? || value > self.maxval
           self.minval = value if self.minval.nil? || value < self.minval
-          self.capture_min = date_val if self.capture_min.nil? || date_val < self.capture_min
-          self.capture_max = date_val if self.capture_max.nil? || date_val > self.capture_max
+          self.capture_min = date_val if self.capture_min.nil? || date_val.to_i < self.capture_min.to_i
+          self.capture_max = date_val if self.capture_max.nil? || date_val.to_i > self.capture_max.to_i
           cnt = index
         end
       end
