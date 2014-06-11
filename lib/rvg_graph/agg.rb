6b5a0ff49c37f9ebdc20a874ac9e14db876d2485
grimm <jason@gina.alaska.edu>
2014-06-10 17:42:52 -0800

    Better date handling in the agg function and better date display format

--- lib/rvg_graph/agg.rb
+++ lib/rvg_graph/agg.rb
@@ -9,16 +9,14 @@ module RvgGraph
     def initialize(data, name, no_data_value)
       name = name.split(",").first
       date ||= data["date"]
+      self.capture_max = data["date"][-1]
+      self.capture_min = data["date"][0]
 
       cnt = 0
       data[name].each_with_index do |value, index|
-        date_val = date.nil? ? index : date[index]
-
         unless check_no_data(value, no_data_value)
           self.maxval = value if self.maxval.nil? || value > self.maxval
           self.minval = value if self.minval.nil? || value < self.minval
-          self.capture_min = date_val if self.capture_min.nil? || date_val.to_i < self.capture_min.to_i
-          self.capture_max = date_val if self.capture_max.nil? || date_val.to_i > self.capture_max.to_i
           cnt = index
         end
       end
